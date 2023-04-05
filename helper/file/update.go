d37b06822485685cca63d4c57f09404baedb9333
mindulle <mindullestudio@gmail.com>
2023-04-05 12:12:43 +0000

    fix: prevent a program be dead if target file doesn't exist

--- helper/file/update.go
+++ helper/file/update.go
@@ -4,20 +4,15 @@ import (
 	"os"
 )
 
-func UpdateFile(filename string, data []byte) error {
-    // Check if the file exists
-    _, err := os.Stat(filename)
-
-    // Create the file if it doesn't exist
-    if os.IsNotExist(err) {
-        _, err := os.Create(filename)
-        if err != nil {
-					panic(err)
-        }
-    }
-
+func UpdateFile(path string,filename string, data []byte) error {
+    // If file or directory doesn't exist, make one.
+	if _, err := os.Stat(path + filename); os.IsNotExist(err) {
+		os.MkdirAll(path, os.ModePerm)
+		os.Create(path + filename)
+	}
+    
     // Open the file with write-only mode and truncate its contents
-    file, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC, 0644)
+    file, err := os.OpenFile(path + filename, os.O_WRONLY|os.O_TRUNC, 0644)
     if err != nil {
 			panic(err)
     }
--- helper/file/update_test.go
+++ helper/file/update_test.go
@@ -3,5 +3,5 @@ package myfile
 import "testing"
 
 func TestUpdateFile(t *testing.T) {
-	UpdateFile("example.txt", []byte("Test for UpdateFile method is done successfully!"))
+	UpdateFile("javacsript/","example.txt", []byte("Test for UpdateFile method is done successfully!"))
 }
