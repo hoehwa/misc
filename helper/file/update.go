ffa932a923dff25906642beb9f22f33d324e78b6
mindulle <mindullestudio@gmail.com>
2023-04-06 11:50:36 +0000

    chore: fix indent

--- helper/file/update.go
+++ helper/file/update.go
@@ -6,7 +6,7 @@ import (
 
 func UpdateFile(path string,filename string, data []byte) error {
     // If file or directory doesn't exist, make one.
-	if _, err := os.Stat(path + filename); os.IsNotExist(err) {
+    if _, err := os.Stat(path + filename); os.IsNotExist(err) {
 		os.MkdirAll(path, os.ModePerm)
 		os.Create(path + filename)
 	}
@@ -14,14 +14,14 @@ func UpdateFile(path string,filename string, data []byte) error {
     // Open the file with write-only mode and truncate its contents
     file, err := os.OpenFile(path + filename, os.O_WRONLY|os.O_TRUNC, 0644)
     if err != nil {
-			panic(err)
+		panic(err)
     }
     defer file.Close()
 
     // Write the data to the file
     _, err = file.Write(data)
     if err != nil {
-			panic(err)
+		panic(err)
     }
 
     return nil
